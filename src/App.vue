c39ee025914833dab88d682159434595cc0a8e41
zhanghao <htzhanghao@hexindai.com>
2021-07-20 14:20:46 +0800

    增加并初始化element

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <h1>八卦浔炉系统</h1>
+    <el-button>八卦浔炉系统</el-button>
+    <el-button icon="el-icon-search" circle></el-button>
     <!-- 路由 -->
     <router-view/>
   </div>
@@ -8,7 +10,12 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+
+  },
+  methods: {},
+  created () {}
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
+// 加载Element组件库
+import ElementPlus from 'element-plus'
+// 引入Element组件库的样式
+import 'element-plus/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
 
 // 最佳全局样式未见
 import './styles/index.less'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(ElementPlus).mount('#app')
